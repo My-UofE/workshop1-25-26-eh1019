005273acc432c892ed6637c26818d93d01fcdf99
eh1019 <eh1019@exeter.ac.uk>
2026-01-16 13:21:53 +0000

    Fleshed out process_guess(), added comments to check_higher_lower()

--- game_functions.py
+++ game_functions.py
@@ -7,13 +7,31 @@ def pick_value(poss_values):
 
 # function to be used in game_2: Higher or Lower
 def check_higher_lower(current_val, next_val, user_input):
+    # If next_val is greater and user is correct
     if (next_val > current_val) and (user_input == "h"):
         return True
+    # If next_val is lesser and user is correct
     elif (next_val < current_val) and (user_input == "l"):
         return True
+    # If user is incorrect
     else:
         return False
 
 # function to be used in game_3: Hangman
 def process_guess(letter, board, word):
-    pass
+    # If letter is in word...
+    if letter in word:
+        # Go through word
+        for i in range(0, len(word)):
+            if word[i] == letter:
+                # Replace each relevant _ in board with letter
+                board[i] = letter
+        
+        # Print result
+        print(f"Well done! '{letter}' is in the word")
+        return True
+    else:
+        # Print results if letter is not in word
+        print(f"Sorry, '{letter}' is not in the word")
+        return False
+        
